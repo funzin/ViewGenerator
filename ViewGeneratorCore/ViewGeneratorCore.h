c422062e32892de4e9a28f86ba2b6fd4635052c5
funzin <famistap@gmail.com>
2019-08-17 13:30:56 +0900

    Create ViewGeneratorCore

---
+++ ViewGeneratorCore/ViewGeneratorCore.h
@@ -0,0 +1,19 @@
+//
+//  ViewGeneratorCore.h
+//  ViewGeneratorCore
+//
+//  Created by 中澤郁斗 on 2019/08/17.
+//  Copyright © 2019 funzin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ViewGeneratorCore.
+FOUNDATION_EXPORT double ViewGeneratorCoreVersionNumber;
+
+//! Project version string for ViewGeneratorCore.
+FOUNDATION_EXPORT const unsigned char ViewGeneratorCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewGeneratorCore/PublicHeader.h>
+
+
