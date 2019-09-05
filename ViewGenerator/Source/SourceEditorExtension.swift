00232b7a74310d9fb57b99c14d2b903e269cdf51
funzin <nakazawa.fumito@gmail.com>
2019-09-06 03:00:54 +0900

    Commentout

--- ViewGenerator/Source/SourceEditorExtension.swift
+++ ViewGenerator/Source/SourceEditorExtension.swift
@@ -11,12 +11,16 @@ import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
 
-    func extensionDidFinishLaunching() {
-        // If your extension needs to do any work at launch, implement this optional method.
-    }
+    /*
+     func extensionDidFinishLaunching() {
+     // If your extension needs to do any work at launch, implement this optional method.
+     }
+     */
 
-    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
-        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-        return []
-    }
+    /*
+     var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+     // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+     return []
+     }
+     */
 }
