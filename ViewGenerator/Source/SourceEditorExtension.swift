e92054a098379e18e71c9d451cd10c21716441ea
funzin <nakazawa.fumito@gmail.com>
2019-09-08 13:57:27 +0900

    Add comment

--- ViewGenerator/Source/SourceEditorExtension.swift
+++ ViewGenerator/Source/SourceEditorExtension.swift
@@ -23,4 +23,11 @@ class SourceEditorExtension: NSObject, XCSourceEditorExtension {
      return []
      }
      */
+
+    /*
+     var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+     // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+     return []
+     }
+     */
 }
